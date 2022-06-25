61e0d2277836a3c740654ca06426780c3cfcee21
eatsiomay <108177471+eatsiomay@users.noreply.github.com>
2022-06-25 17:40:56 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,187 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  <style type="text/css">
+    body {
+      font-family: Montserrat;
+    }
+
+    p {
+      font-family: Montserrat;
+    }
+
+    h1 {
+      font-family: Montserrat;
+    }
+
+    h2 {
+      font-family: Montserrat;
+    }
+  </style>
+</body>
+
+<!-- navbar -->
+<nav class="sticky-top navbar navbar-expand-lg navbar-dark" style="background-color: #a52a2a;">
+  <div class="container ms-auto">
+    <a class="navbar-brand" href="#">
+      <img src="img/logo.png" class="imgstyle" alt="logo"><b> EATSIOMAY2.0</b>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">PRODUK</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">TESTIMONI</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">PREORDER</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="#">ALAMAT</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div style="background-color: #ffd9ad;" class="">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+    <path fill="#a52a2a" fill-opacity="1" d="M0,160L21.8,149.3C43.6,139,87,117,131,112C174.5,107,218,117,262,133.3C305.5,149,349,171,393,165.3C436.4,160,480,128,524,112C567.3,96,611,96,655,96C698.2,96,742,96,785,122.7C829.1,149,873,203,916,197.3C960,192,1004,128,1047,101.3C1090.9,75,1135,85,1178,74.7C1221.8,64,1265,32,1309,32C1352.7,32,1396,64,1418,80L1440,96L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path>
+  </svg>
+  <!-- endnavbar -->
+
+  <!-- jumbotron -->
+  <div style="background-color: #ff9ad;" class="">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class=" jumbotron jumbotron-fluid">
+            <div class="container ">
+              <h1 class="display-3 jumbotext">LIFE IS BETTER WITH SIOMAY</h1>
+              <p class="lead"><img src="img/side3.png" align="right">Siomay merupakan salah satu makanan yang mudah sekali kita temui. Makanan ini identik dengan bumbu kacang dan rasa gurih ikan tenggiri. Bumbu ini dibuat dari kacang tanah, bawang merah, bawang putih, gula pasir, asam Jawa, garam dan cuka yang dihaluskan menjadi satu kemudian dicampur dengan air. Siomay biasanya dimakan dengan beberapa campuran bahan makanan, seperti kentang, kubis, telur ayam rebus, dan tahu.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#ffd9ad" fill-opacity="1" d="M0,256L30,240C60,224,120,192,180,176C240,160,300,160,360,181.3C420,203,480,245,540,272C600,299,660,309,720,272C780,235,840,149,900,144C960,139,1020,213,1080,229.3C1140,245,1200,203,1260,165.3C1320,128,1380,96,1410,80L1440,64L1440,0L1410,0C1380,0,1320,0,1260,0C1200,0,1140,0,1080,0C1020,0,960,0,900,0C840,0,780,0,720,0C660,0,600,0,540,0C480,0,420,0,360,0C300,0,240,0,180,0C120,0,60,0,30,0L0,0Z"></path>
+</svg>
+<!-- endjumbotron -->
+
+<!-- Product -->
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <div class="mt-5 mb-5 jumbotron jumbotron-fluid">
+        <div class="container ">
+          <h1 class="display-3 jumbotext">OUR PRODUCTS</h1>
+          <div class="container">
+            <div class="row justify-content-evenly">
+
+              <div class="col-md-4 mb-3">
+                <div class="card1">
+                  <img src="img/2.jpeg" class="card-img-top" alt="Projects-1" />
+                  <div class="card-body">
+                    <a class="h3 text-light text-center">Produk Siomay</a><br>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-4 mb-3">
+                <div class="card1">
+                  <img src="img/3.jpeg" class="card-img-top" alt="Projects-2" />
+                  <div class="card-body">
+                    <a class="h3 text-light text-center">Produk Siomay</a><br>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-4 mb-3">
+                <div class="card1">
+                  <img src="img/4.jpeg" class="card-img-top" alt="Projects-3" />
+                  <div class="card-body">
+                    <a class="h3 text-light text-center">Produk Siomay</a><br>
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#ffffff" fill-opacity="1" d="M0,96L21.8,80C43.6,64,87,32,131,42.7C174.5,53,218,107,262,160C305.5,213,349,267,393,288C436.4,309,480,299,524,266.7C567.3,235,611,181,655,144C698.2,107,742,85,785,112C829.1,139,873,213,916,256C960,299,1004,309,1047,272C1090.9,235,1135,149,1178,128C1221.8,107,1265,149,1309,149.3C1352.7,149,1396,107,1418,85.3L1440,64L1440,0L1418.2,0C1396.4,0,1353,0,1309,0C1265.5,0,1222,0,1178,0C1134.5,0,1091,0,1047,0C1003.6,0,960,0,916,0C872.7,0,829,0,785,0C741.8,0,698,0,655,0C610.9,0,567,0,524,0C480,0,436,0,393,0C349.1,0,305,0,262,0C218.2,0,175,0,131,0C87.3,0,44,0,22,0L0,0Z"></path>
+</svg>
+<!-- endproduct -->
+
+<!-- footer -->
+<div style="background-color: #EEBFBF;" class=" container-fluid">
+
+  <footer style="margin-top: 100px" class="row row-cols-5 py-5 border-danger border-5 border-top">
+    <div class="col ">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32">
+          <use xlink:href="#bootstrap" />
+        </svg>
+      </a>
+      <p class="text-muted">&copy; Copyright 2022 tokokomputerx.com</p>
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+      <h5>Customer Care</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a class="nav-link p-0 text-muted ">Klaim dan Garansi Produk, silakan hubungi :</a></li>
+        <li class="nav-item mb-2"><a href="mailto:rma.tokokomputerx@tugasuas.com" class="nav-link p-0 ">rma.tokokomputerx@tugasuas.com</a></li>
+        <li class="nav-item mb-2"><a href="https://wa.me/6289199245212311131" class="nav-link p-0 ">WA 089199245212311131</a></li>
+      </ul>
+    </div>
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+      <h5>Toko Kami</h5>
+      <ul class="nav flex-column ">
+        <li class="nav-item mb-2"><a class="nav-link p-0 text-muted ">Buka Senin-Sabtu, 10:00-18:00 WIB</a></li>
+        <li class="nav-item mb-2"><a href="https://goo.gl/maps/dbwCcAuBcgDXoHDs9" class=" nav-link p-0 ">Mangga Dua Mall Lt.3 No. 31-32 Jakarta Pusat 10730</a></li>
+        <li class="nav-item mb-2"><a href="https://wa.me/628998725557225212" class="nav-link p-0 ">WA 08998725557225212</a></li>
+        <li class="nav-item mb-2"><a href="mailto:salestokox@tugasuas.com" class="nav-link p-0 ">salestokox@tugasuas.com</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+<!-- endfooter -->
+
+</html>
